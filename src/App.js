89cba29c0cf95a45cc628d1046d10cf938197613
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-01-30 22:19:54 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,33 +1,100 @@
 import "./App.css";
 import { useState } from "react";
-import Axios from "axios"
+import Axios from "axios";
+import Recipetile from "./recipetile/Recipetile";
 
 function App() {
-  const YOUR_APP_ID = "API_ID";
-  const YOUR_APP_KEY = "API_KEY";
-  const [recipes, setRecipes] = useState([])
+  const YOUR_APP_ID = "APP_ID";
+  const YOUR_APP_KEY = "APP_KEY";
+  const [query, setQuery] = useState("");
+  const [healthLabel, setHealthLabel] = useState("vegetarian");
+  const [recipes, setRecipes] = useState([]);
+
   const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
+
   const getRecipeInfo = async () => {
-    let result = await Axios.get(url);
+    var result = await Axios.get(url);
     setRecipes(result.data.hits);
-    console.log(result);
-  }
+    console.log(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault(); 
+    getRecipeInfo();
+  };
+
   return (
     <div className="app">
-      <h1>
-        <u>Food Recibe Hub</u>
+      <h1 onClick={getRecipeInfo}>
+        <u>Food Recipe Hub</u>🥗
       </h1>
-      <div className="app__searchForm">
+      <form className="app__searchForm" onSubmit={onSubmit}>
         <input
-          className="app__input"
           type="text"
           placeholder="Type the Ingredient"
           autoComplete="Off"
+          className="app__input"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
         />
         <select className="app__healthLabels">
-          <option value="vegan">vegan</option>
+          <option
+            value="vegan"
+            onClick={() => {
+              setHealthLabel("vegan");
+            }}
+          >
+            vegan
+          </option>
+          <option
+            value="vegetarian"
+            onClick={() => {
+              setHealthLabel("vegetarian");
+            }}
+          >
+            vegetarian
+          </option>
+          <option
+            value="low-sugar"
+            onClick={() => {
+              setHealthLabel("low-sugar");
+            }}
+          >
+            low-sugar
+          </option>
+          <option
+            value="dairy-free"
+            onClick={() => {
+              setHealthLabel("dairy-free");
+            }}
+          >
+            dairy-free
+          </option>
+          <option
+            value=" immuno-supportive "
+            onClick={() => {
+              setHealthLabel(" immuno-supportive ");
+            }}
+          >
+            immuno-supportive
+          </option>
+          <option
+            value="wheat-free"
+            onClick={() => {
+              setHealthLabel("wheat-free");
+            }}
+          >
+            wheat-free
+          </option>
         </select>
-        <input className="app__submit" value="Get Recipe" type="submit" />
+        <input type="submit" value="Get Recipe" className="app__submit" />
+      </form>
+      <div className="app__recipes">
+        {recipes.map((recipe) => {
+          return <Recipetile recipe={recipe} />;
+        })}
       </div>
     </div>
   );
