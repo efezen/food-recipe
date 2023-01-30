8eabfa17834c1de7ff3d73f948fadc3b1498a2a9
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-01-30 15:32:11 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios"
 
 function App() {
+  const YOUR_APP_ID = "API_ID";
+  const YOUR_APP_KEY = "API_KEY";
+  const [recipes, setRecipes] = useState([])
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
+  const getRecipeInfo = async () => {
+    let result = await Axios.get(url);
+    setRecipes(result.data.hits);
+    console.log(result);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>
+        <u>Food Recibe Hub</u>
+      </h1>
+      <div className="app__searchForm">
+        <input
+          className="app__input"
+          type="text"
+          placeholder="Type the Ingredient"
+          autoComplete="Off"
+        />
+        <select className="app__healthLabels">
+          <option value="vegan">vegan</option>
+        </select>
+        <input className="app__submit" value="Get Recipe" type="submit" />
+      </div>
     </div>
   );
 }
